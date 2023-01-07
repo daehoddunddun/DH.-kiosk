5a23de99c1296feff02b60a78a4fd9e3bcd02f1a
kimdaeho <eogh773@naver.com>
2023-01-07 21:38:06 +0900

    01/07 Nexs.JS study& setting

---
+++ pages/login/index.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <>
+      <div>아아악</div>
+    </>
+  );
+};
+
+export default Login;
---
+++ pages/product/index.tsx
@@ -0,0 +1,9 @@
+const Prouduct = () => {
+  return (
+    <>
+      <div>제품정보</div>
+    </>
+  );
+};
+
+export default Prouduct;
