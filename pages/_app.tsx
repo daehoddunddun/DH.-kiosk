36ecd45f3a51c9022c2f792415039b80ed74b6cc
kimdaeho <eogh773@naver.com>
2023-01-14 03:13:59 +0900

    01/14 skeleton css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import {
   useRecoilState,
   useRecoilValue,
 } from "recoil";
-import "../styles/globals.css";
+import "../styles/reset.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,9 @@
-import Link from "next/link";
 import Head from "next/head";
-import styled from "styled-components";
+import Link from "next/link";
+import css from "styled-jsx/css";
 import { useMediaQuery } from "react-responsive";
 import { useState } from "react";
 import { useRecoilState } from "recoil";
-
-const Main = styled.div`
-  width: 1000px;
-  height: 1000px;
-  background-color: black;
-`;
-
-const Test = styled.img`
-  width: 1000px;
-`;
-
-const Navs = styled.p`
-  width: 100%;
-  height: 20px;
-  background-color: black;
-  font-size: 30px;
-  color: blue;
-`;
 
 export default function index() {
   return (
@@ -29,10 +11,49 @@ export default function index() {
       <Head>
         <title>DH.Kiosk</title>
       </Head>
-      <Main>
-        <Navs>ss</Navs>
-        <Test src="/virtual.gif" />
-      </Main>
+      <style jsx>{style}</style>
+      <div className="main-wrap">
+        <div className="main-item">메인 이미지</div>
+      </div>
+      <div className="order-wrap">
+        <button className="order-btn">
+          <Link href="/product">주문하기</Link>
+        </button>
+      </div>
     </>
   );
 }
+
+const style = css`
+  .main-wrap {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 850px;
+    background-color: #9aafaf;
+  }
+  .main-item {
+    background: red;
+    width: 90%;
+    height: 98%;
+  }
+
+  .order-wrap {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 170px;
+    background: #bee6bb;
+  }
+
+  .order-btn {
+    width: 80%;
+    height: 50%;
+    font-size: 30px;
+    border-radius: 50px;
+    border: 1px solid #eb5feb;
+    background: #eb5feb;
+  }
+`;
